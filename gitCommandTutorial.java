42bbd1b1664e12951bf59267d788ac22d35815eb
Jason Huang <jason.huang@ucalgary.ca>
2020-09-14 10:50:05 -0600

    Added print line feature

--- gitCommandTutorial.java
+++ gitCommandTutorial.java
@@ -1,6 +1,7 @@
 public class gitCommandTutorial {
   public static void main (String[] args){
     System.out.println("Hello World!");
-
+	System.out.println("|");
+	System.out.println("This feature is added for the addLineFeature branch");
   }
 }
